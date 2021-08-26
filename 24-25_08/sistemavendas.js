9d97a236cf8397010540ed88db661b8c7e878d30
Serafim <luiz_311002@hotmail.com>
2021-08-25 21:06:03 -0300

    24-25_08

--- 24-25_08/sistemavendas.js
+++ 24-25_08/sistemavendas.js
@@ -24,12 +24,33 @@ let sistemaVendas = () =>{
                 else {
                     alert(`Vendedor já existe com este código`)	
                 }
-                break
-        case 2:
-                break
-        case 3: alert(`O programa será encerrado`)
-                break
-        default: alert(`Opção inválida`)
+
+                 console.log(vetVendedores)
+                    break
+            case 2: let objeto = {
+                        codigo: Number(prompt(`Informe código do vendedor`)),
+                        mes: Number(prompt(`Informe mês da venda`)),
+                        valor: Number(prompt(`Informe o valor da venda`)) 
+                    }
+                    let achou = false
+                    for(let i=0;i<vetVendas.length;i++){
+                        
+                    } for(let i=0;i<vetVendas.length;i++){
+                        if ((vetVendas[i].codigo == objeto1.codigo) && (vetVendas[i].mes == objeto1.mes)){
+                            achou1 = true // encontrei - não podemos cadastrar venda
+                        }
+                    }
+                    if (!achou1){
+                        vetVendas.push(objeto1)
+                    }
+                    else {
+                        alert(`Já existe vende deste vendedor neste mês`)
+                    }
+                    console.log(vetVendas)
+                    break
+            case 3: alert(`O programa será encerrado`)
+                    break
+            default: alert(`Opção inválida`)
     }
 }
 while (opcao != 3)
